264d720a235175dd3cec4b6cfa02499befce4fe9
ryanyny <ryanfebriansyah060207@gmail.com>
2025-09-16 22:59:14 +0700

    connect react to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [msg, setMsg] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+    .then(res => res.text())
+    .then(data => setMsg(data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +27,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <h1>{msg}</h1>
     </div>
   );
 }
